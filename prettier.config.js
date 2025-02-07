59b5e5542891e091cbf23e8c683fea48b36f6b1d
Anton Isakov <isakovanton@mail.ru>
2025-02-06 20:43:01 -0800

    [NPM] Add Tailwind plugin

--- prettier.config.js
+++ prettier.config.js
@@ -6,6 +6,8 @@ const config = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/index.css',
 }
 
 export default config
